3c8713225c7d66179755873ff3e8036f96162cc3
Austin Poulson <austinpoulson@gmail.com>
2023-06-05 21:15:12 -0500

    Switch API to snoowrap

--- Spreddit.js
+++ Spreddit.js
@@ -1,72 +1,61 @@
 import { credentials } from './Keys.js';
-import axios from 'axios';
+import snoowrap from 'snoowrap';
 import { entries, subreddits } from './Config.js';
 
 // Authenticate with Reddit API
 async function authenticate(clientId, clientSecret, username, password, userAgent) {
   try {
-    const response = await axios.post('https://www.reddit.com/api/v1/access_token', 
-      `grant_type=password&username=${username}&password=${password}`,
-      {
-        auth: {
-          username: clientId,
-          password: clientSecret
-        },
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'User-Agent': userAgent
-        }
-      }
-    );
-    return response.data.access_token;
+    return response = new snoowrap({
+      userAgent,
+      clientId,
+      clientSecret,
+      username,
+      password
+    });
   } catch (error) {
-    console.error('Failed to authenticate:', error.message);
+    interfaceLog('Failed to authenticate:' + error);
   }
 }
 
 // Make a post on Reddit
-async function makePost(accessToken, subreddit, title, content, userAgent) {
-  try {
-    const response = await axios.post(`https://oauth.reddit.com/r/${subreddit}/api/submit`, 
-      {
-        title: title,
-        kind: 'link',
-        url: content
-      },
-      {
-        headers: {
-          'Authorization': `Bearer ${accessToken}`,
-          'User-Agent': userAgent
-        }
-      }
-    );
-    interfaceLog(response);
-  } catch (error) {
-    return error;
-  }
+async function makePost(client, subreddit, title, content) {
+  client.getSubreddit(subreddit).submitLink({
+    title,
+    url: content
+  }).then((post) => {
+
+    interfaceLog('Post submitted successfully');
+    return post;
+  }).catch((err) => {
+
+    interfaceLog('Error submitting post');
+    throw err;
+  });
 }
 
+// Authenticate with an account on the list, then post from that account
 async function postFromRandomAccount(subreddit, title, content) {
   interfaceLog('Authenticating...');
   const {clientId, clientSecret, username, password, userAgent} = credentials[Math.floor(Math.random()*credentials.length)]
-  const accessToken = await authenticate(clientId, clientSecret, username, password, userAgent);
-  if (!accessToken) {
+  const client = await authenticate(clientId, clientSecret, username, password, userAgent);
+  if (!client) {
     interfaceLog('Authentication Failed');
     return
   }
-  interfaceLog('Authentication Passed: ' + accessToken);
-  makePost(accessToken, subreddit, title, content, userAgent)
-    .then((response) => {return response})
+  interfaceLog('Authentication Passed');
+  return makePost(client, subreddit, title, content)
     .catch((error) => {return error});
 }
 
+// Format the entry into a title
 function formatTitle(entry, format) {
   for (var key in entry) {
-    format = format.replace(key, entry.key);
+    format = format.replace(key, entry[key]);
   }
   return format;
 }
 
+// Logging for the interface. This way we can differentiate between interface logs and debug logs.
 function interfaceLog(log){
   console.log(log);
 }
@@ -76,22 +65,31 @@ function interfaceLog(log){
 
   interfaceLog('Starting...');
   entries.forEach((entry) => {
+
     interfaceLog('Rolling dice');
-    const chance = (Math.random() * 11);
-    if (chance < entry.score) {
+    const chance = (Math.random() * 10);
+    if (chance <= entry.score) {
+
       interfaceLog('Dice roll passed, checking subreddit');
       const subredditName = entry.subreddits[Math.floor(Math.random()*entry.subreddits.length)];
       const subredditObj = subreddits.find(item => item.name === subredditName);
-      if (subredditObj.restrictions !== []) {
-        interfaceLog('Checking subreddit posting requirements');
-        // TODO set limitations here
+      if (subredditObj) {
+        if (subredditObj.restrictions !== []) {
+
+          interfaceLog('Checking subreddit posting requirements');
+          // TODO set limitations here
+
+        }
+        const title = formatTitle(entry, subredditObj.format);
+        const content = entry.links[Math.floor(Math.random()*entry.links.length)];
+        console.log(title);
+        postFromRandomAccount(subredditName, title, content)
+          .then(() => {
+            interfaceLog('Pausing for rate limit');
+            setTimeout(() => {}, 600000);
+          })
+          .catch((error) => interfaceLog(error));
       }
-      const title = formatTitle(entry, subredditObj.format);
-      const content = entry.links[Math.floor(Math.random()*entry.links.length)];
-      postFromRandomAccount(subredditName, title, content)
-        .then(setTimeout(() => {}, 600000))
-        .then((response) => console.log(response))
-        .catch((error) => console.log(error));
     }
   })
 
